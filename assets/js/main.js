00c8b6bcbd1bee6b070261de3e0cc6d82877aec1
Vikas Sharma <devuser@ing-fe102.dev>
2016-06-28 13:54:48 +0530

    new algo added

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 function SelectionSort() {
 
     var arr = document.getElementById('inputArr').value.split(',');
-    document.getElementsByTagName('code')[0].innerText = ''
+    document.getElementsByTagName('pre')[0].innerText = ''
     // main logic starts from here
     var min = 0;
     for (var i = 0; i < arr.length; i++)
@@ -18,34 +18,52 @@ function SelectionSort() {
             var temp = arr[i];
             arr[i] = arr[min];
             arr[min] = temp;
-            document.getElementsByTagName('code')[0].innerText += arr + '\n';
+            document.getElementsByTagName('pre')[0].innerText += arr + '\n';
         }
         
     }
-            
-    //document.getElementById('inputArr').value = arr.join(',');
-    //console.log(arr)
 }
 
 function BubbleSort() {
 
     var arr = document.getElementById('inputArr').value.split(',');
-    document.getElementsByTagName('code')[0].innerText = ''
+    document.getElementsByTagName('pre')[0].innerText = ''
 
     // main logic starts from here
     for (var i = 0; i < arr.length; i++) {
 
-        for (var j = i; j < arr.length; j++) {
-            if (parseInt(arr[j]) >parseInt( arr[j + 1]))
+        for (var j = 0; j < arr.length; j++) {
+
+            if (parseInt(arr[j]) > parseInt( arr[j + 1]))
             {
                 var temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
-                document.getElementsByTagName('code')[0].innerText += arr + '\n';
+                document.getElementsByTagName('pre')[0].innerText += arr + '\n';
             }
         }        
     }
+}
 
-    //document.getElementById('inputArr').value = arr.join(',');
-    //console.log(arr)
+
+function InsertionSort() {
+
+    var arr = document.getElementById('inputArr').value.split(',');
+    document.getElementsByTagName('pre')[0].innerText = ''
+
+    // main logic starts from here
+    for (var i = 0; i < arr.length; i++) {
+
+        var pointer = parseInt(arr[i]);
+        var j=i-1;
+
+        while(j>=0 && parseInt(arr[j]) > pointer){
+            arr[j+1]=arr[j];
+            j=j-1;
+            
+        }
+        arr[j+1]=pointer;
+        document.getElementsByTagName('pre')[0].innerText += arr + '\n';
+                
+    }
 }
